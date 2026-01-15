e16985051e0157fc8d19e1fe13e26dab1e62ae7b
dzungpham <23127003@student.hcmus.edu.vn>
2026-01-15 15:30:08 +0700

    feat(dataset): add FST dataset and collate enhancements
    Introduce FontDatasetFST for dual-mode training, add enhanced CollateFN handling
    variable shapes and SCR negatives, and provide debug utilities for FST
    debugging.

--- dataset/__init__.py
+++ dataset/__init__.py
@@ -1,2 +1,6 @@
-from .font_dataset import *
-from .collate_fn import *
+from .font_dataset import FontDataset
+from .collate_fn import CollateFN
+
+from .font_dataset_fst import FontDataset as FontDatasetFST
+from .font_dataset_fst import FontDatasetDebug
+from .collate_fn_fst import CollateFNFST
