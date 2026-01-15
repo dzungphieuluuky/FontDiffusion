fc6560b931554ef8b4ff53e6e947296470abd563
dzungpham <23127003@student.hcmus.edu.vn>
2026-01-15 19:43:58 +0700

    plaplapal

--- dataset/__init__.py
+++ dataset/__init__.py
@@ -1,6 +1,5 @@
 from .font_dataset import FontDataset
 from .collate_fn import CollateFN
 
-from .font_dataset_fst import FontDataset as FontDatasetFST
-from .font_dataset_fst import FontDatasetDebug
-from .collate_fn_fst import CollateFNFST
+from .font_dataset_fst import FontDataset as FontDatasetFST, FontDatasetDebug
+from .collate_fn_fst import CollateFN as CollateFNFST
