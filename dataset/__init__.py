fee50c144adbedacf91a234fc6b27f23fe5a997d
dzungpham <23127003@student.hcmus.edu.vn>
2026-01-18 23:02:46 +0700

    refactor: remove legacy dataset and collate modules

--- dataset/__init__.py
+++
@@ -1,5 +0,0 @@
-from .font_dataset import FontDataset
-from .collate_fn import CollateFN
-
-from .font_dataset_fst import FontDataset as FontDatasetFST, FontDatasetDebug
-from .collate_fn_fst import CollateFN as CollateFNFST
