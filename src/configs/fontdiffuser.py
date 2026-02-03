a09bf98c2ab1ef32471bcec83ad905ba8073b9bf
dzungpham <23127003@student.hcmus.edu.vn>
2026-02-03 16:36:25 +0700

    feat(config): enable SE attention by default

--- src/configs/fontdiffuser.py
+++ src/configs/fontdiffuser.py
@@ -82,7 +82,7 @@ def get_parser():
     model_group.add_argument(
         "--channel_attn",
         action="store_true",
-        default=False,
+        default=True,
         help="Use SE attention in U-Net",
     )
     model_group.add_argument(
